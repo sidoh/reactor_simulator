cd46b06866eaec79fece568772f06d675a4eee84
Christopher <cmullins@liveramp.com>
2014-12-27 22:46:38 -0800

    start on thrift types

---
+++ src/main/thrift/simulator.thrift
@@ -0,0 +1,11 @@
+namespace java org.sidoh.reactor_simulator.types
+
+struct ReactorResult {
+  1: double efficiency;
+  2: double output;
+  3: double fuelFertility;
+  4: double coolantTemperature;
+  5: double fuelHeat;
+  6: double reactorHeat;
+  7: string reactorLayout
+}

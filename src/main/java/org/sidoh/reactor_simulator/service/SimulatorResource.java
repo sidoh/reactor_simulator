6c107698413ff31569da766c112809e08a3a0478
Christopher <cmullins@liveramp.com>
2014-12-31 07:57:01 -0800

    add validation

--- src/main/java/org/sidoh/reactor_simulator/service/SimulatorResource.java
+++ src/main/java/org/sidoh/reactor_simulator/service/SimulatorResource.java
@@ -9,6 +9,8 @@ import restx.annotations.RestxResource;
 import restx.factory.Component;
 import restx.security.PermitAll;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 @Component
 @RestxResource
 public class SimulatorResource {
@@ -17,6 +19,8 @@ public class SimulatorResource {
   @PermitAll
   @GET("/simulate")
   public ReactorResult simulate(ReactorDefinition definition) {
+    validateReactorDefinition(definition);
+
     BigReactorSimulator simulator = new BigReactorSimulator(
         definition.isActivelyCooled(),
         MAX_NUMBER_OF_TICKS
@@ -40,6 +44,19 @@ public class SimulatorResource {
   }
 
   private static void validateReactorDefinition(ReactorDefinition reactorDefinition) {
+    checkArgument(reactorDefinition.getxSize() >= 5, "xSize should be at least 5");
+    checkArgument(reactorDefinition.getzSize() >= 5, "zSize should be at least 5");
+    checkArgument(reactorDefinition.getHeight() >= 3, "zSize should be at least 3");
 
+    final int layoutSize = (reactorDefinition.getxSize() - 2) * (reactorDefinition.getzSize() - 2);
+    checkArgument(
+        reactorDefinition.getLayout().length() == layoutSize,
+        String.format(
+            "layout size for a %dx%d reactor should be %d",
+            reactorDefinition.getxSize(),
+            reactorDefinition.getzSize(),
+            layoutSize
+        )
+    );
   }
 }

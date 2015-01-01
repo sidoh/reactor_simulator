89d67d5cfadcf16837f9c508d657abb73c4a001f
Christopher <cmullins@liveramp.com>
2015-01-01 10:46:43 -0800

    remove thrift, embedded source

--- src/main/java/erogenousbeef/bigreactors/gui/controls/grab/IBeefGuiGrabbable.java
+++
@@ -1,9 +0,0 @@
-package erogenousbeef.bigreactors.gui.controls.grab;
-
-import net.minecraft.util.IIcon;
-
-public interface IBeefGuiGrabbable {
-
-	public IIcon getIcon();
-	public String getName();
-}
--- src/main/java/erogenousbeef/bigreactors/simulator/IRadiationModeratorSimulator.java
+++
@@ -1,8 +0,0 @@
-package erogenousbeef.bigreactors.simulator;
-
-import erogenousbeef.bigreactors.common.data.RadiationData;
-import erogenousbeef.bigreactors.common.data.RadiationPacket;
-
-public interface IRadiationModeratorSimulator {
-	public void moderateRadiation(RadiationData returnData, RadiationPacket radiation, IFakeReactorWorld world, MultiblockReactorSimulator reactor);
-}

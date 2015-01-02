80ca47a4373a5ef62a63383eb3f1147cff0a5805
Christopher <cmullins@liveramp.com>
2015-01-02 09:10:32 -0800

    Revert "measure stability from heat, not RF output"
    This reverts commit e7c3772776b272d8b0f2f8520cebd61e1646ba96.

--- src/main/java/org/sidoh/reactor_simulator/simulator/BigReactorSimulator.java
+++ src/main/java/org/sidoh/reactor_simulator/simulator/BigReactorSimulator.java
@@ -123,10 +123,10 @@ public class BigReactorSimulator {
     for (int i = 0; i < this.ticks; i++) {
       simulator.updateServer();
 
-      final double heatValue = simulator.getFuelHeat();
-      final double heatDelta = (heatValue - lastValue);
+      final double energyValue = simulator.getEnergyGeneratedLastTick();
+      final double energyDelta  = (energyValue - lastValue);
 
-      if (heatDelta < 0) {
+      if (energyDelta < 0) {
         numNegativeDeltas++;
       }
 
@@ -134,7 +134,7 @@ public class BigReactorSimulator {
         break;
       }
 
-      lastValue = heatValue;
+      lastValue = energyValue;
     }
 
     return new ReactorResult(
